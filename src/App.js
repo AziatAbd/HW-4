af67d2a51be8485e5c048701d0465bc463a9ad01
Aziat <abdimalikovaziat@gmail.com>
2023-01-09 16:37:18 +0600

    HomeWork-4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/new-expense/NewExpense";
+
+const expenses = [
+  {
+    id: "a1",
+    title: "bumaga",
+    price: 300,
+    date: new Date(),
+  },
+  {
+    id: "a2",
+    title: "zaryadka",
+    price: 243,
+    date: new Date(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
